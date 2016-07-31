2cec73f7256a7e44d1e7f4ba9aec51d731719a80
Joey Grisafe <jgrisafe@users.noreply.github.com>
2016-07-31 09:39:44 -0700

    created config template

---
+++ shopify_config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    shop: 'MYSHOP.myshopify.com ',
+    shopify_api_key: 'xxxx', // Your API key 
+    access_token: 'xxxx', // Your API password
+    verbose: false // if you don't want to see headers on every call
+  }
